4b97419f57e5d0cded284327f172002ff938e5de
azizkale <azizkale@hotmail.com>
2023-07-10 17:43:22 +0200

    fix/apollo-server-express instead of apollo-server

--- src/index.js
+++ src/index.js
@@ -1,48 +1,49 @@
-import { ApolloServer } from 'apollo-server'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import express from 'express'
-import http from 'http'
-import { expressMiddleware } from '@apollo/server/express4'
-import resolvers from './schema/resolvers/index.js'
-import typeDefs from './schema/typeDefs/index.js'
+import { ApolloServer } from 'apollo-server-express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+import resolvers from './schema/resolvers/index.js';
+import typeDefs from './schema/typeDefs/index.js';
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT || 3000
-mongoose
-  .connect(process.env.MONGODB_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(async () => {
-    const app = express()
-    const httpServer = http.createServer(app)
+const PORT = process.env.PORT || 3000;
 
-    const server = new ApolloServer({
-      typeDefs,
-      resolvers,
-    })
+(async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
 
-    await server.listen()
+        const app = express();
+        app.use(express.json());
 
-    await app.use(
-      '/graphql',
-      cors({
-        origin: ['http://localhost:4200', 'http://192.168.0.17:4200'],
-      }),
-      expressMiddleware(server),
-    )
+        const server = new ApolloServer({
+            typeDefs,
+            resolvers,
+        });
 
-    app.get('/api', (req, res) => {
-      res.send('api works!')
-    })
+        await server.start();
 
-    await httpServer.listen(PORT, () => {
-      console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`)
-    })
-  })
-  .catch((error) => {
-    console.error('Failed to connect to MongoDB:', error)
-  })
+        server.applyMiddleware({
+            app,
+            cors: {
+                origin: ['http://localhost:4200', 'http://192.168.0.17:4200'],
+            },
+        });
+
+        app.get('/api', (req, res) => {
+            res.send('api works!');
+        });
+
+        const httpServer = http.createServer(app);
+        httpServer.listen(PORT, () => {
+            console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+        });
+    } catch (error) {
+        console.error('Failed to connect to MongoDB:', error);
+    }
+})();
