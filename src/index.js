a321ed2fb8683e266c272cd05eb51b07bd8608c9
azizkale <azizkale@hotmail.com>
2023-07-10 19:04:23 +0200

    feat/engines vesions in package.json

--- src/index.js
+++ src/index.js
@@ -1,48 +1,48 @@
-import { ApolloServer } from 'apollo-server-express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import express from 'express';
-import http from 'http';
-import resolvers from './schema/resolvers/index.js';
-import typeDefs from './schema/typeDefs/index.js';
+import { ApolloServer } from 'apollo-server-express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import express from 'express'
+import http from 'http'
+import resolvers from './schema/resolvers/index.js'
+import typeDefs from './schema/typeDefs/index.js'
 
-dotenv.config();
+dotenv.config()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-(async() => {
-    try {
-        await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+;(async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 
-        const app = express();
-        app.use(express.json());
+    const app = express()
+    app.use(express.json())
 
-        const server = new ApolloServer({
-            typeDefs,
-            resolvers,
-        });
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+    })
 
-        await server.start();
+    await server.start()
 
-        server.applyMiddleware({
-            app,
-            cors: {
-                origin: ['http://localhost:4200', 'http://192.168.0.17:4200'],
-            },
-        });
+    server.applyMiddleware({
+      app,
+      cors: {
+        origin: ['http://localhost:4200', 'http://192.168.0.17:4200'],
+      },
+    })
 
-        app.get('/api', (req, res) => {
-            res.send('api works!');
-        });
+    app.get('/api', (req, res) => {
+      res.send('api works!')
+    })
 
-        const httpServer = http.createServer(app);
-        httpServer.listen(PORT, () => {
-            console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
-        });
-    } catch (error) {
-        console.error('Failed to connect to MongoDB:', error);
-    }
-})();
+    const httpServer = http.createServer(app)
+    httpServer.listen(PORT, () => {
+      console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`)
+    })
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error)
+  }
+})()
